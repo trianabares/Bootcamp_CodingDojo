95aebae913cad52d5b405359acd8b6be25677c62
Triana Bares <68872407+trianabares@users.noreply.github.com>
2023-07-12 02:43:22 -0300

    Eliminar archivos

--- JavaFun/PrimerPrograma.java
+++
@@ -1,7 +0,0 @@
-public class PrimerPrograma {
-    public static void main(String[] args){
-        System.out.println("Vivo en San José de Mayo");
-        System.out.println("Tengo 24 años de edad");
-        System.out.println("Vivo en San José de Mayo");
-    }
-}
